d133b9be77a1b74da971b2cb99aa482abe59c8b1
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-10-31 15:27:17 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,39 @@
+const cekHariKerja = (day) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      const dataDay = ['senin','selasa','rabu','kamis','jumat']
+
+      let cek = dataDay.find((item) => {
+        return item === day
+      })
+
+      if(cek){
+        resolve(cek)
+      } else {
+        reject(new Error('Hari ini bukan hari kerja'))
+      }
+
+    },3000)
+  })
+};
+
+
+// a. then catch
+cekHariKerja('selasa')
+  .then(result => console.log(`Hari ${result} adalah hari kerja`))
+  .catch(error => console.log(error.message))
+
+
+// b. try catch
+async function getResult(str){
+  try{
+    let result = await cekHariKerja(str)
+    console.log(`Hari ${result} adalah hari kerja`)
+  }catch(error){
+    console.log(error.message)
+  }
+}
+
+getResult('jumat')
+getResult('minggu')
